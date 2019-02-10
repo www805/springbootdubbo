22d29161a314faa9e5bdf8452fea97ee6c68d4e6
zhuang <289590076@qq.com>
2019-02-10 17:38:33 +0800

    版本二

--- dubbo-demo/src/main/java/com/itheima/controller/IndexController.java
+++ dubbo-demo/src/main/java/com/itheima/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.itheima.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,11 @@ public class IndexController {
     @RequestMapping(value = "/index")
     public String show(String name){
         return name;
+    }
+
+    @RequestMapping(value = "/go/{id}")
+    public String haha(@PathVariable int id){
+        return "你输入变量是：" + id;
     }
 
 }
